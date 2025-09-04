69e04efb3c35eeab0395ceb517e7e2e7eeaa0bd1
davinzifc <47566127+davinzifc@users.noreply.github.com>
2025-09-04 15:46:03 -0500

    ✨ feat(app): initialize result management module with basic structure and configurations

---
+++ server/result-management/src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60 * 1000,
+        limit: 400,
+      },
+    ]),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      cache: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
+})
+export class AppModule {}
---
+++ server/result-management/src/main.ts
@@ -0,0 +1,65 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+  const app = await NestFactory.create(AppModule);
+
+  app.use(
+    helmet({
+      contentSecurityPolicy: false,
+      hsts: {
+        maxAge: 31536000,
+        includeSubDomains: true,
+        preload: true,
+      },
+      noSniff: true,
+      xssFilter: true,
+      frameguard: { action: 'deny' },
+      hidePoweredBy: true,
+      crossOriginEmbedderPolicy: false,
+      referrerPolicy: false,
+    }),
+  );
+
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('CLARISA Result Management API')
+    .setDescription('API for managing CLARISA results')
+    .setVersion('1.0')
+    .addTag('results')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('CL_PORT');
+
+  if (!port) {
+    throw new Error('The port is not defined in environment variables');
+  }
+  await app.listen(port).then(() => {
+    logger.debug(`Application is running on: http://localhost:${port}`);
+    logger.debug(`Swagger is running on: http://localhost:${port}/api/docs`);
+  });
+}
+bootstrap();
